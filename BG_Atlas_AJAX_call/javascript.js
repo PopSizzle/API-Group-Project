d0f9337272ea3e30c2cfe211c5b465407aa80a25
Sam Poppe <poppe.samuel@gmail.com>
2020-04-01 11:59:58 -0700

    add functionality for category search

--- BG_Atlas_AJAX_call/javascript.js
+++ BG_Atlas_AJAX_call/javascript.js
@@ -1,14 +1,14 @@
-var gameBox = $("#gameInfoBox")
+
 var minPlayers = "";
 var maxPlayers = "";
-var maxPrice = "&lt_msrp=" + 1000;
-
-var mechanicID = "fBOTEBUAmV";
-var categoryID = "ODWOjWAJj3";
+var maxPrice = "";
+var workerPlace = "fBOTEBUAmV";
+var varPlayerPowers = "XM2FYZmBHH";
 
 function searchGames(){
     event.preventDefault();
 
+    
     var name = "name=" + $("#nameInput").val().trim();
     var exactName = false;
 
@@ -16,11 +16,6 @@ function searchGames(){
         name = name + "&exact=true";
         console.log(name);
     }
-
-    addMinPlayers();
-    addMaxPlayers();
-    addMaxPrice();
-    addMechanic();
     
     var queryURL = "https://www.boardgameatlas.com/api/search?" + name + minPlayers + maxPlayers + maxPrice + "&client_id=NHfcsxreTb";
     console.log(queryURL);
@@ -29,12 +24,14 @@ function searchGames(){
         url: queryURL,
         method: "GET"
     }).then(function(response){
-        gameBox.empty();
+        $("#gameInfoBox").empty();
         console.log(response);
 
+            var mechSearch = 
+
             for( var i = 0; i<response.games.length; i++){
-                if(checkCategory(response.games[i], categoryID) === true && checkMechanics(response.games[i], mechanicID) === true){
-                        console.log("worker Placement");
+                if(checkMechanics(response.games[i], varPlayerPowers) === true){
+                        console.log("Player Powers");
                         var header = $("<h3>");
                         header.text(response.games[i].name);
                         var img = $("<img>");
@@ -46,39 +43,39 @@ function searchGames(){
         })
 }
 
-$("#searchGames").on("click", searchGames);
+$("#addName").on("click", searchGames);
 
-function addMinPlayers(){
+$("#addMinPlayers").on("click", function(){
+    event.preventDefault();
     var number = parseInt($("#minPlayersInput").val().trim());
     minPlayers = "&gt_min_players=" + (number - 1);
     $("#minPlayersDisplay").text("Minimum Players: " + number);
     console.log(minPlayers);
-}
+});
 
-function addMaxPlayers(){
+$("#addMaxPlayers").on("click", function(){
+    event.preventDefault();
     var number = parseInt($("#maxPlayersInput").val().trim());
     maxPlayers = "&lt_max_players=" + (number + 1);
     $("#maxPlayersDisplay").text("Maximum Players: " + number)
     console.log(maxPlayers);
-}
+})
 
-function addMaxPrice(){
+$("#addMaxPrice").on("click", function(){
     event.preventDefault();
-    if($("#maxPriceInput").val()){
-        var number = parseInt($("#maxPriceInput").val().trim());
-        maxPrice = "&lt_msrp=" + (number + 0.01);
-        $("#maxPriceDisplay").text("Maximum Price (MSRP): $" + number);
-    }
+    var number = parseInt($("#maxPriceInput").val().trim());
+    maxPrice = "&lt_msrp=" + (number + 0.01);
+    $("#maxPriceDisplay").text("Maximum Price (MSRP): $" + number);
     console.log(maxPrice);
-}
+})
 
-function addMechanic(){
+$("#addMechanic").on("click", function(){
     event.preventDefault();
     var mechanic = $("#mechanicInput").val().trim();
     
     $("#mechanicDisplay").text("Mechanic: " + mechanic);
     console.log(mechanic);
-}
+})
 
 function checkMechanics(game, mechID){
     var array = game.mechanics;
@@ -87,17 +84,4 @@ function checkMechanics(game, mechID){
             return true;
         }
     }
-}
-
-function checkCategory(game, catID){
-    var array = game.categories;
-    for(var i=0; i<array.length; i++){
-        if (array[i].id === catID){
-            return true;
-        }
-    }
-}
-
-function clearSearch(){
-    gameBox.empty
 }
