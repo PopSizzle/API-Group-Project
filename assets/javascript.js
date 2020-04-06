ca26e641a9870b8032b0ae81a1595662db1eeb42
CrisTerry <crisdc88@gmail.com>
2020-04-05 19:56:15 -0700

    Add youtube link to model

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,6 @@
 // Declare all variables, including calls to ids
 var gamesData;
+var gamesYT_URL;
 var gameBox = $("#gameInfoBox");
 var searchButton = $("#searchGames");
 var clearButton = $("#clearSearch");
@@ -23,13 +24,14 @@ var videoType = "Game Review"
 
 
 // Function to run when search executes
-function searchGames(){
+function searchGames() {
+    gamesYT_URL = [];
     event.preventDefault();
 
     var name = "name=" + nameInput.val().trim();
     var exactName = false;
 
-    if(exactName === true){
+    if (exactName === true) {
         name = name + "&exact=true";
         console.log(name);
     }
@@ -46,57 +48,81 @@ function searchGames(){
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response){
+    }).then(function (response) {
         // Empty Display Box
         gameBox.empty();
 
-        console.log(response);
+
 
         // by Cristina:  added the response to global variable so I can access it for the Modal.
         gamesData = response.games;
-            // For each game returned, check if category and mechanics preferences are matched
-            for( var i = 0; i<6; i++){
-                var card = "#card" + i;
-                $(card).removeClass("hidden");
+        // For each game returned, check if category and mechanics preferences are matched
+        for (var i = 0; i < 6; i++) {
+            var card = "#card" + i;
+            $(card).removeClass("hidden");
 
-                var img = "#cardImage" + i;
-                var year = "#cardYear" + i;
-                var title = "#cardTitle" + i;
-                var info = "#cardInfo" + i;
-                var descript = "#cardDescript" + i;
+            var img = "#cardImage" + i;
+            var year = "#cardYear" + i;
+            var title = "#cardTitle" + i;
+            var info = "#cardInfo" + i;
+            var descript = "#cardDescript" + i;
 
-                var minNumber = response.games[i].min_players;
-                var maxNumber = response.games[i].max_players;
-                var minTime = response.games[i].min_playtime;
-                var maxTime = response.games[i].max_playtime;
-                var shortDescript = response.games[i].description_preview.split(" ").splice(0,50).join(" ");
+            var minNumber = response.games[i].min_players;
+            var maxNumber = response.games[i].max_players;
+            var minTime = response.games[i].min_playtime;
+            var maxTime = response.games[i].max_playtime;
+            var shortDescript = response.games[i].description_preview.split(" ").splice(0, 50).join(" ");
 
-                $(img).attr("src", response.games[i].images.medium);
-                $(year).text(response.games[i].year_published);
-                $(title).text(response.games[i].name);
-                $(info).text("---Players: " + minNumber + "-" + maxNumber + " Playtime: " + minTime + "-" + maxTime + "minutes");
-                $(descript).text(shortDescript);
-            }    
-        
-        })
+            $(img).attr("src", response.games[i].images.medium);
+            $(year).text(response.games[i].year_published);
+            $(title).text(response.games[i].name);
+            response.games[i]["ytURL"] = "";
+
+            var queryURL2 = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + videoType + response.games[i].name + "&type=video&key=AIzaSyAjs8I4xGPzoBBcuCk4afKvx-IRoVaQX0A"
+
+            console.log(queryURL2);
+            var yt_url = "";
+
+            $.ajax({
+                url: queryURL2,
+                method: "GET",
+                async: false,
+                success: function (youtubeResponse) {
+                    console.log('youtubeResponse', youtubeResponse);
+                    var videoId = youtubeResponse.items[0].id.videoId;
+                    yt_url = "https://www.youtube.com/embed/" + videoId;
+                }
+            })
+            response.games[i].ytURL = yt_url;
+            console.log("object : " + response.games[i].ytURL);
+
+
+
+            $(info).text("---Players: " + minNumber + "-" + maxNumber + " Playtime: " + minTime + "-" + maxTime + "minutes");
+            $(descript).text(shortDescript);
+        }
+
+        console.log("1st response gamesData:  ", gamesData);
+        console.log("array of urls" + gamesYT_URL);
+    })
 }
 // Function to add minimum player parameter
-function addMinPlayers(){
+function addMinPlayers() {
     var number = parseInt(minPlayersInput.val().trim());
     minPlayers = "&gt_min_players=" + (number - 1);
     minPlayersDisplay.text("Minimum Players: " + number);
     console.log(minPlayers);
 }
 // Function to add maximum player parameter
-function addMaxPlayers(){
+function addMaxPlayers() {
     var number = parseInt(maxPlayersInput.val().trim());
     maxPlayers = "&lt_max_players=" + (number + 1);
     maxPlayersDisplay.text("Maximum Players: " + number)
     console.log(maxPlayers);
 }
 // function to add maximum MSRP parameter
-function addMaxPrice(){
-    if($("#maxPriceInput").val()){
+function addMaxPrice() {
+    if ($("#maxPriceInput").val()) {
         var number = parseInt(maxPriceInput.val().trim());
         maxPrice = "&lt_msrp=" + (number + 0.01);
         maxPriceDisplay.text("Maximum Price (MSRP): $" + number);
@@ -104,49 +130,50 @@ function addMaxPrice(){
     console.log(maxPrice);
 }
 // Function to add mechanic to the screen ***NEEDS UPDATING TO LINK TO MECHANICS IDs***
-function addMechanic(){
+function addMechanic() {
     var mechanic = mechanicInput.find('option:selected').attr('data-id');
-    if(mechanic === "none"){
+    if (mechanic === "none") {
         mechanicID = "";
         console.log(mechanicID);
     }
-    else{
+    else {
         mechanicID = "&mechanics=" + mechanic;
         mechanicDisplay.text("Mechanic: " + mechanic);
         console.log(mechanicID);
     }
 }
 // Function to add category to the screen *** NEEDS UPDATING TO LINK TO CATEGORY IDs***
-function addCategory(){
+function addCategory() {
     var category = categoryInput.find('option:selected').attr('data-id');
-    if(category === "none"){
+    if (category === "none") {
         categoryID = "";
     }
-    else{categoryID = "&categories=" + category;
+    else {
+        categoryID = "&categories=" + category;
         categoryDisplay.text("Category: " + category);
         console.log(categoryID);
-    }   
+    }
 }
 // Function to check if mechanics match
-function checkMechanics(game, mechID){
+function checkMechanics(game, mechID) {
     var array = game.mechanics;
-    for(var j = 0; j < array.length; j++){
-        if(array[j].id === mechID){
+    for (var j = 0; j < array.length; j++) {
+        if (array[j].id === mechID) {
             return true;
         }
     }
 }
 // Function to check if categories match
-function checkCategory(game, catID){
+function checkCategory(game, catID) {
     var array = game.categories;
-    for(var i=0; i<array.length; i++){
-        if (array[i].id === catID){
+    for (var i = 0; i < array.length; i++) {
+        if (array[i].id === catID) {
             return true;
         }
     }
 }
 // Function to clear search box and display box
-function clearSearch(){
+function clearSearch() {
     event.preventDefault();
 
     gameBox.empty();
@@ -158,26 +185,28 @@ function clearSearch(){
     $("#categoryInput").val("0");
     console.log("clear");
 
-    for(var i=0; i<6; i++){
-        
+    for (var i = 0; i < 6; i++) {
+
         var card = $("#card" + i);
         console.log(card);
         card.addClass("hidden");
     }
 }
 // Function to search Youtube
-function youtubeResponse(selectedGame){
+function youtubeResponse(selectedGame) {
     var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + videoType + selectedGame + "&type=video&key=AIzaSyAjs8I4xGPzoBBcuCk4afKvx-IRoVaQX0A"
 
     console.log(queryURL);
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response){
+    }).then(function (response) {
 
         var videoId = response.items[0].id.videoId;
-        
-        return "https://www.youtube.com/embed/" + videoId;
+
+        var yt_url = "https://www.youtube.com/embed/" + videoId;
+        console.log("url inside call: " + yt_url);
+        return yt_url;
     })
 }
 //         for ( var i = 0; i < 6; i++) {
